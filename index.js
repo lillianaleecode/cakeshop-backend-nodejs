7242c37ba22b335db4b07d85eb7762f40547d03c
lillianaleecode <lilliana.lee@code.berlin>
2022-04-07 00:33:30 +0200

    connecting MysQl with a small testing

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const app = express()
 
+const mysql = require('mysql')
+
+const db = mysql.createPool({
+    host: 'localhost', 
+    user: 'root',
+    password: 'ORACLE545901',
+    database: 'online_store',
+});
+
 app.get('/', (req, res) => {
-    res.send("hello lilly and all!!");
+    const sqlInsert = 
+     "INSERT INTO `online_store`.`products` (`product_id`, `name`, `unit_measure_id`, `price_per_unit`) VALUES ('11', 'sel', '1', '13');"
+
+
+    db.query(sqlInsert, (err, result)=>{
+        res.send("hello lilly and all!!");
+
+    })
+   
 });
 
 app.listen(3001, () => {
