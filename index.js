6820b6832960886e431794d693aabf7d5e475e5e
lillianaleecode <lilliana.lee@code.berlin>
2022-04-08 14:22:15 +0200

    fixed the connection frontend with backend

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser'); //help format everything to json
 
+const PORT = process.env.PORT || 3001;
+
 const cors = require('cors');
 
 const mysql = require('mysql2');
@@ -19,17 +21,40 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/api/insert', (req, res)=> {
+    
     const cakeName = req.body.cakeName;
     const cakeReview = req.body.cakeReview;
 
 
-    const sqlInsert = "INSERT INTO cake_shop.cake_review (id, name, review) VALUES (?,?,?))"
-    db.query(sqlInsert, [cakeName, cakeReview], (err, result)=>{
+    const sqlInsert = 
+    "INSERT INTO cake_review (name, review) VALUES (?,?)";
+    db.query(sqlInsert, [cakeName, cakeReview], 
+        (err, result)=>{
         //called con the front end by submitReview
-        console.log(err);
+        if (err) {
+            console.log("oh no" + err);
+        } else {
+            res.send("values Inserted");
+            console.log(result);
+        };
+        
 
     });
 });
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
+  
+
+
+app.listen(PORT, () => {
+    console.log("running on port 3001");
+
+});
+
+//run the file: node index.js
+//
 
 /* app.get("/", (req, res) => {
     const sqlInsert = 
@@ -42,11 +67,3 @@ app.post('/api/insert', (req, res)=> {
     })
    
 }); */
-
-app.listen(3001, () => {
-    console.log("running on port 3001");
-
-});
-
-//run the file: node index.js
-//
