645b26106d86c96e7c3ce9220521fa3dc98a963e
lillianaleecode <lilliana.lee@code.berlin>
2022-04-08 12:23:33 +0200

    progress on CRUD-create

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser'); //help format everything to json
 
-const mysql = require('mysql2')
+const cors = require('cors');
+
+const mysql = require('mysql2');
 
 const db = mysql.createPool({
     host: 'localhost', 
@@ -10,7 +13,25 @@ const db = mysql.createPool({
     database: 'cake_shop',
 });
 
-app.get("/", (req, res) => {
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/insert', (req, res)=> {
+    const cakeName = req.body.cakeName;
+    const cakeReview = req.body.cakeReview;
+
+
+    const sqlInsert = "INSERT INTO cake_shop.cake_review (id, name, review) VALUES (?,?,?))"
+    db.query(sqlInsert, [cakeName, cakeReview], (err, result)=>{
+        //called con the front end by submitReview
+        console.log(err);
+
+    });
+});
+
+/* app.get("/", (req, res) => {
     const sqlInsert = 
     "INSERT INTO cake_shop.cake_review (id, name, review) VALUES ('1', '2', '1')"
 
@@ -20,7 +41,7 @@ app.get("/", (req, res) => {
 
     })
    
-});
+}); */
 
 app.listen(3001, () => {
     console.log("running on port 3001");
