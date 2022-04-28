96cda18dc7104fbdc8fcd8b13833a8a966d33e7f
lillianaleecode <lilliana.lee@code.berlin>
2022-04-28 15:33:00 +0200

    adding MongoDB to the project (for the blogs)

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const Blog = require('./models/blog')
+
+//connect to mongodb
+
+//const dbURI = 'mongodb+srv://root:1234@node.zn6hp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://root:1234@node.zn6hp.mongodb.net/node?retryWrites=true&w=majority';
+mongoose.connect(dbURI)
+.then ((result) => app.listen(5001))
+.catch((err)=> console.log(err));
 
 const PORT = process.env.PORT || 3001;
 
@@ -7,9 +17,26 @@ const cors = require('cors');
 
 const mysql = require('mysql2');
 
+
+
 const dotenv = require('dotenv'); //for the login
 const cookieParser = require('cookie-parser');//for the login
 
+//mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'new blog',
+        snippet:'about my blog',
+        body: 'more!'
+    });
+    blog.save()
+    .then((result) => {
+        res.send(result)
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+})
 
 //invocar motor de plantilla  
 app.set('view engine', 'ejs');
