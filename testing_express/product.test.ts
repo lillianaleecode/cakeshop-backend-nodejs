47a3a1345909f40c9ad93b18d47e32d163505a5b
lillianaleecode <lilliana.lee@code.berlin>
2022-05-06 14:32:23 +0200

    some testing (progress)

---
+++ testing_express/product.test.ts
@@ -0,0 +1,11 @@
+describe('product', () =>{
+    describe('get user roles route', () => {
+        describe('given the user does not exist', () => {
+            it("should return a 404" , () => {
+                expect(true).toBe(true);
+                
+            });
+
+        })
+    })
+})
---
+++ testing_express/server.js
@@ -0,0 +1,12 @@
+import app from './app.js'
+// //moved to app.js file 
+// import express from 'express';
+
+// const app = express();
+
+// app.get('/test', (req, res) => {
+//     res.send("yeyy")
+
+// });
+
+app.listen(8080, () => console.log("listening on port 8080 :)"))
